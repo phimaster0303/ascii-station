32d6e0caae1ef508439ea4e6b7e2a2fccc1354f1
phimaster0303 <94635072+phimaster0303@users.noreply.github.com>
2022-03-01 17:33:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
-import pyfiglet, art, simple_chalk as chalk, random
-
-
+import pyfiglet, art, simple_chalk as chalk, random, os
+chalk.black(os.system("cls"))
+chalk.black(os.system("pip install pyfiglet"))
+chalk.black(os.system("cls"))
+chalk.black(os.system("pip install art"))
+chalk.black(os.system("cls"))
+chalk.black(os.system("pip install simple_chalk"))
+chalk.black(os.system("cls"))
+chalk.black(os.system("pip install os"))
+chalk.black(os.system("cls"))
 #Ask what text2ascii libary you want to use
-print("What do you want to use?:")
+print(chalk.blueBright("What do you want to use?:"))
 print((chalk.green("[P]yfiglet ")+ chalk.blue("|") + chalk.red(" [A]rt")))
 asciitype = input("")
 
 
 if asciitype.upper() == "P":
-    randomfont = input("Do you want to use a random font? [Y]es | [N]o: ")
+    randomfont = input(chalk.cyan("Do you want to use a random font? [Y]es | [N]o: "))
     if randomfont.upper() == "Y":
         fig = pyfiglet.Figlet()
         fonts = fig.getFonts()
         while True:
             rfont = random.choice(fonts)
-            text = input("What text do you want to convert to ascii art?: ")
+            text = input(chalk.magenta("What text do you want to convert to ascii art?: "))
             print(pyfiglet.figlet_format(text,font=rfont))
             print(rfont)
     else:
         while True:
             ifont = input("What font do you want to use: ")
-            itext = input("What text do you want to convert to ascii art: ")
+            itext = input(chalk.magenta("What text do you want to convert to ascii art?: "))
             pyfiglet.figlet_format(itext, font=ifont)
 elif asciitype.upper() == "A":
     print("What size do you want the ascii art to be? | 1 -> small | 2 -> medium | 3 -> large | 4 -> X-large")
     asciisize = input()
     while True:
-        match asciisize:
-            case "1":
-                asciitext = input("What text do you want to convert to ascii art?: ")
-                art.tprint(asciitext, font="random-small")
-            case "2":
-                asciitext = input("What text do you want to convert to ascii art?: ")
-                art.tprint(asciitext, font="random-medium")
-            case "3":
-                asciitext = input("What text do you want to convert to ascii art?: ")
-                art.tprint(asciitext, font="large")
-            case "4":
-                asciitext = input("What text do you want to convert to ascii art?: ")
-                art.tprint(asciitext, font="random-xlarge")
+        if asciisize == "1":
+            asciitext = input("What text do you want to convert to ascii art?: ")
+            art.tprint(asciitext, font="random-small")
+        elif asciisize == "2":
+            asciitext = input("What text do you want to convert to ascii art?: ")
+            art.tprint(asciitext, font="random-medium")
+        elif asciisize == "3":
+            asciitext = input("What text do you want to convert to ascii art?: ")
+            art.tprint(asciitext, font="large")
+        elif asciisize == "4":
+            asciitext = input("What text do you want to convert to ascii art?: ")
+            art.tprint(asciitext, font="random-xlarge")
